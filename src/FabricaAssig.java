18ae8d4bceb2cda6e52cbe4dec18a493116d7417
Rahuvich <raulmateob@gmail.com>
2018-10-25 13:56:06 +0200

    Modificando FabricaAssig

--- src/FabricaAssig.java
+++ src/FabricaAssig.java
@@ -1,14 +1,52 @@
 import org.json.*;
 
+import java.util.ArrayList;
+
 public class FabricaAssig {
 
-    /*JSONObject obj = new JSONObject(" .... ");
+    public static void readAssig(String archivo){
+        JSONObject assig = new JSONObject(archivo);
+        JSONArray lista = assig.getJSONArray("Assignatures");
+
+        for(int i = 0; i < assig.length(); ++i){
+            String nomAssig = lista.getJSONObject(i).getString("codiAssig");
+            String numBloc = lista.getJSONObject(i).getString("bloc");
+
+        }
+    }
+
+    /*
+
+    JSON FILE:
+
+    {
+   "pageInfo": {
+         "pageName": "abc",
+         "pagePic": "http://example.com/content.jpg"
+    }
+    "posts": [
+         {
+              "post_id": "123456789012_123456789012",
+              "actor_id": "1234567890",
+              "picOfPersonWhoPosted": "http://example.com/photo.jpg",
+              "nameOfPersonWhoPosted": "Jane Doe",
+              "message": "Sounds cool. Can't wait to see it!",
+              "likesCount": "2",
+              "comments": [],
+              "timeOfPost": "1234567890"
+         }
+    ]
+}
+
+
+    JAVA FILE:
+
+    JSONObject obj = new JSONObject(" .... ");
     String pageName = obj.getJSONObject("pageInfo").getString("pageName");
 
     JSONArray arr = obj.getJSONArray("posts");
-for (int i = 0; i < arr.length(); i++)
+    for (int i = 0; i < arr.length(); i++)
     {
         String post_id = arr.getJSONObject(i).getString("post_id");
-    ......
     }*/
 }
